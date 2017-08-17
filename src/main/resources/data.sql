eff4d64b36a270091366fe79fc4d103606cfb48b
jinge <jinge@wacai.com>
2017-08-18 07:07:28 +0800

    add db sql files and .yaml config file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user (id, account, name, age, balance) values (1, 'account1', 'zhangsan', 20, 100.00);
+insert into user (id, account, name, age, balance) values (2, 'account2', 'lisi', 28, 180.00);
+insert into user (id, account, name, age, balance) values (3, 'account3', 'wangwu', 32, 280.00);
+insert into user (id, account, name, age, balance) values (4, 'account4', 'xiaoer', 35, 2380.00);
+insert into user (id, account, name, age, balance) values (5, 'account5', 'laojiang', 132, 2080.00);
