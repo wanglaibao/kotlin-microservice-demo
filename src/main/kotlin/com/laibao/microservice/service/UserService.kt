31064b4d9bbef3135804088c77434677fcb51310
jinge <jinge@wacai.com>
2017-10-17 06:22:02 +0800

    update

--- src/main/kotlin/com/laibao/microservice/service/UserService.kt
+++ src/main/kotlin/com/laibao/microservice/service/UserService.kt
@@ -3,12 +3,14 @@ package com.laibao.microservice.service
 import com.laibao.microservice.entity.User
 import com.laibao.microservice.repository.UserRepository
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PathVariable
 
 @Service
 class UserService (val userRepository: UserRepository){
-    fun findUserByUserId(@PathVariable id: Long?): User {
+
+    fun findUserByUserId(id: Long?): User {
         val user: User = userRepository.findOne(id)
         return user
     }
+
+    fun findAllUser(): List<User> = userRepository.findAll()
 }
